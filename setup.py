6677b6a23d55cd90a4120aa6532d9bd1fac7bbc3
lxn3032 <lxn3032@corp.netease.com>
2018-01-05 15:41:47 +0800

    bump up version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ reqs = [str(ir.req) for ir in install_reqs if ir.req]
 
 setup(
     name='hrpc',
-    version='1.0.2',
+    version='1.0.3',
     keywords="hrpc rpc",
     description='A common interface based RPC framework',
     packages=find_packages(),
